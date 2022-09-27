a01926a1f429c39aa7d3d4a7cd9d68f1b9525ead
mapKey <690112316@qq.com>
2022-09-27 17:52:00 +0800

    tttt

---
+++ OS_Linux_code/cmake-build-debug/CMakeFiles/OS_Linux_code.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS_Linux_code.dir/main.c.obj"
+  "OS_Linux_code.exe"
+  "OS_Linux_code.exe.manifest"
+  "OS_Linux_code.pdb"
+  "libOS_Linux_code.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OS_Linux_code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
